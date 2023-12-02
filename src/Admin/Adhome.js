5b440f80ff6496bf5312642cabc7461e91e6a37b
toon63 <kannatthanan@gmail.com>
2023-12-02 12:15:06 +0700

    first_time

---
+++ src/Admin/Adhome.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Sidebar from './Sidebar';
+
+
+function Adhome() {
+  return (
+    <div>
+    <Sidebar />
+    <div className="content">
+      <h2>Main Content</h2>
+      <p>Content goes here...</p>
+    </div>
+  </div>
+);
+};
+
+export default Adhome
