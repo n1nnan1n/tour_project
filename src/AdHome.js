3154cb85931acae3c23fb2e98782043213132e28
n1nnan1n <thanin.cwtnk@gmail.com>
2023-12-06 16:40:54 +0700

    resize

--- src/AdHome.js
+++ src/AdHome.js
@@ -11,7 +11,7 @@ function AdHome() {
 
   return (
     <>
-      <AppBar position="fixed" sx={{ width: `calc(100% - ${drawerWidth}px)`, ml: `${drawerWidth}px` }}>
+      <AppBar position="fixed" sx={{ width: `calc(100% - ${drawerWidth}px)`, ml: `${drawerWidth}px` }} >
         <Toolbar>
           <Typography variant="h6" noWrap component="div">
             Home
@@ -20,19 +20,19 @@ function AdHome() {
       </AppBar>
       
 
-      <Grid container rowSpacing={3.5} columnSpacing={2.75} style={{marginLeft:'250px',maxWidth:'1240px'}}>
+      <Grid container rowSpacing={3.5} columnSpacing={2.75} style={{marginLeft:'250px',maxWidth:'',width:"150vh"}}>
       
       {/* row 1 */}
       {/* <Grid item xs={9} >
         <Typography variant="h5">Dashboard</Typography>
       </Grid> */}
-      <Grid item xs={12} sm={6} md={4} lg={4}>
+      <Grid item xs={12} sm={6} md={4} lg={3}>
         <AnalyticEcommerce title="Total Page Views" count="4,42,236" percentage={59.3} extra="35,000" />
       </Grid>
-      <Grid item xs={12} sm={6} md={4} lg={4}>
+      <Grid item xs={12} sm={6} md={4} lg={3}>
         <AnalyticEcommerce title="Total Users" count="78,250" percentage={70.5} extra="8,900" />
       </Grid>
-      <Grid item xs={12} sm={6} md={4} lg={4}>
+      <Grid item xs={12} sm={6} md={4} lg={3}>
         <AnalyticEcommerce title="Total Order" count="18,800" percentage={27.4} isLoss color="warning" extra="1,943" />
       </Grid>
      
@@ -43,7 +43,7 @@ function AdHome() {
         <Grid item xs={12} md={7} lg={8}>
           <Grid container alignItems="center" justifyContent="space-between">
             <Grid item>
-              <Typography variant="h5">Recent Orders</Typography>
+              <Typography variant="h4">Recent Orders</Typography>
             </Grid>
             <Grid item />
           </Grid>
