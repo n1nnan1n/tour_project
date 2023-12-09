79a18301164e72c949142c3389f64544009ce9c2
n1nnan1n <thanin.cwtnk@gmail.com>
2023-12-09 22:42:36 +0700

    switch id

--- controllers/CreateCheckout.js
+++ controllers/CreateCheckout.js
@@ -4,26 +4,49 @@ const createCheckoutSession = async (req, res) => {
     const YOUR_DOMAIN = 'http://localhost:3000';
     // const YOUR_DOMAIN = 'https://tour-project-git-toon-n1nnan1ns-projects.vercel.app';
     try {
-    // const { user_id, tour_id, quantity, tour_date, tour_price, total_price } = req.body;
-    const { quantity } = req.body;
+    const { user_id, tour_id, quantity, tour_date} = req.body;
     const session = await stripe.checkout.sessions.create({
       ui_mode: 'embedded',
       line_items: [
         {
           // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-          price: 'price_1OJUqLDiyx2jx89TjLCVacym',
-        //   userID : user_id,
-        //   userID : tour_id,
-        //   tourdate: tour_date,
-        //   price: total_price,
+          price: getProductPriceId(tour_id),
           quantity: quantity,
         },
       ],
       mode: 'payment',
+      metadata: {
+        userID : user_id,
+        tourID : tour_id,
+        tourdate: tour_date
+      },
       return_url: `${YOUR_DOMAIN}/return?session_id={CHECKOUT_SESSION_ID}`,
     });
   
     res.send({clientSecret: session.client_secret});
+
+    function getProductPriceId(tour_id) {
+        if (tour_id === '655a2fae9b4c465647c4609a') {
+          return 'price_1OJUqLDiyx2jx89TjLCVacym';
+        } else if (tour_id === '655e1317a5dd819be5edeb6d') {
+          return 'price_1OL92ZDiyx2jx89TqeBcJgBx';
+        } else if (tour_id === '655e1533a5dd819be5edeb6e') {
+          return 'price_1OL93YDiyx2jx89TQyGvTpuN';
+        }else if (tour_id === '655ee85bdc69725f99120b84') {
+          return 'price_1OL944Diyx2jx89TVwzjBgHf';
+        }else if (tour_id === '655ee996dc69725f99120b85') {
+          return 'price_1OL94MDiyx2jx89TtA9JiYbF';
+        }else if (tour_id === '655eee2fdc69725f99120b86') {
+          return 'price_1OL94lDiyx2jx89TeBH2d8QG';
+        }else if (tour_id === '655eef3adc69725f99120b87') {
+          return 'price_1OL95CDiyx2jx89T9ZzotPUw';
+        }else if (tour_id === '655ef143dc69725f99120b88') {
+          return 'price_1OL95dDiyx2jx89TDpRfl6yn';
+        }else if (tour_id === '655ef329dc69725f99120b89') {
+          return 'price_1OL95xDiyx2jx89Tcemfmfbu';
+        }
+      }
+
 } catch (error) {
     console.error('Error during checkout:', error);
     res.status(500).json({ error: 'Internal Server Error' });
