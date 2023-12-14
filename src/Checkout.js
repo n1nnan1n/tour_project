aec58c5803ef3d18eb0edba0f42502a83ad0c180
n1nnan1n <thanin.cwtnk@gmail.com>
2023-12-14 12:11:35 +0700

    order detail

--- src/Checkout.js
+++ src/Checkout.js
@@ -26,6 +26,8 @@ const CheckoutForm = () => {
   const [a_tour_date, seta_tour_date] = useState('');
   const [a_total_price, seta_total_price] = useState('');
 
+  const [order_id, setorder_id] = useState('');
+
   const prepareData = {
     order_user_id: location.state.order_userID,
     order_user_email: location.state.order_user_email,
@@ -35,12 +37,22 @@ const CheckoutForm = () => {
     order_tour_price: location.state.order_tourprice,
     order_total_price: location.state.order_totalprice
   };
-  console.log({"prepare data":prepareData});
-  
-  const originalDate = new Date(prepareData.order_tour_date);
-  const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-  const formattedDate = originalDate.toLocaleDateString("en-GB", options);
 
+  const [orderedDetail, setOrderedDetail] = useState({
+    ordered_status: '',
+    ordered_title: '',
+    ordered_user_id: '',
+    ordered_user_firstname: '',
+    ordered_user_lastname: '',
+    ordered_user_email: '',
+    ordered_tour_id: '',
+    ordered_tour_name: '',
+    ordered_tour_price: '',
+    ordered_tour_date: '',
+    ordered_quantity: '',
+    ordered_total_price: '',
+    ordered_createdAt: ''
+  });
 
   useEffect(() => {
     axios.post("http://localhost:3001/create-checkout-session", prepareData)
@@ -67,15 +79,45 @@ const handleComplete = async () => {
     };
 
     // Wait for the axios.post to complete before proceeding
-    await axios.post('http://localhost:3001/successorder', orderdata);
+    const response = await axios.post('http://localhost:3001/successorder', orderdata);
+    const responseData = response.data;
     // Any code here will run after the axios.post is complete
+    console.log(responseData.order_id);
+    setorder_id(responseData.order_id);
+
+    const orderdetail = await axios.get('http://localhost:3001/getuserorder/'+responseData.order_id);
+    const orderdetailData = orderdetail.data;
+    
+    setOrderedDetail({
+      ordered_status: orderdetailData.order_status,
+      ordered_title: orderdetailData.title,
+      ordered_user_id: orderdetailData.user_id,
+      ordered_user_firstname: orderdetailData.user_firstname,
+      ordered_user_lastname: orderdetailData.user_lastname,
+      ordered_user_email: orderdetailData.user_email,
+      ordered_tour_id: orderdetailData.tour_id,
+      ordered_tour_name: orderdetailData.tour_name,
+      ordered_tour_price: orderdetailData.tour_price,
+      ordered_tour_date: orderdetailData.tour_date,
+      ordered_quantity: orderdetailData.quantity,
+      ordered_total_price: orderdetailData.total_price,
+      ordered_createdAt: orderdetailData.createdAt
+    });
+
     setIsComplete(true);
   } catch (error) {
     console.error('Error posting order data:', error);
   }
 };
+const receieveTourDate = new Date(prepareData.order_tour_date);
+const originalTourDate = new Date(orderedDetail.ordered_tour_date);
+const originalCreatedAt = new Date(orderedDetail.ordered_createdAt);
 
+const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
 
+const formattedreceieveTourDate = receieveTourDate.toLocaleDateString('en-US', options);
+const formattedTourDate = originalTourDate.toLocaleDateString('en-US', options);
+const formattedCreatedAt = originalCreatedAt.toLocaleDateString('en-US', options);
 
 return (
   <div id="checkout">
@@ -85,7 +127,7 @@ return (
         options={{ clientSecret, onComplete: handleComplete }}
       >
         <div style={{ width: '100%', padding: '2%' }}>
-          <h2>[ Name ] : {location.state.order_title} {location.state.order_user_firstname} {location.state.order_user_lastname}<br></br>[ Tour name ] : {location.state.order_tour_name}<br></br>[ Tour date ] : {formattedDate}<br></br>[ Person(s) ] : {prepareData.order_quantity}</h2>
+          <h2>[ Name ] : {location.state.order_title} {location.state.order_user_firstname} {location.state.order_user_lastname}<br></br>[ Tour name ] : {location.state.order_tour_name}<br></br>[ Tour date ] : {formattedreceieveTourDate}<br></br>[ Person(s) ] : {prepareData.order_quantity}</h2>
         </div>
         <EmbeddedCheckout />
       </EmbeddedCheckoutProvider>
@@ -94,6 +136,8 @@ return (
     {isComplete && (
       <div>
         <h2>Payment successfully completed!</h2>
+        <h2>Order ID : {order_id}<br></br>Name : {orderedDetail.ordered_title} {orderedDetail.ordered_user_firstname} {orderedDetail.ordered_user_lastname}<br></br>Email : {orderedDetail.ordered_user_email}<br></br>Tour name : {orderedDetail.ordered_tour_name}<br></br>Person(s) : {orderedDetail.ordered_quantity}<br></br>Total price : {orderedDetail.ordered_total_price}</h2>
+
       </div>
     )}
   </div>
