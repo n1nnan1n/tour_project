c8a6be9eaa4ed6053442231098b674b068b6987d
toon63 <kannatthanan@gmail.com>
2023-12-25 22:59:41 +0700

    test

--- src/About.js
+++ src/About.js
@@ -116,7 +116,7 @@ export default function About() {
               /></div> 
               </Col>
         <Col>
-         <div >
+         {/* <div > */}<div className='none'>
             <h1 className='detailabout'>
                    Let Beyond the Trails be your first choice for unique private tours in and around Bangkok. For example, Thaka floating market, where you can immerse yourself in the local culture as the neighbourhood vendors ply their wares from their floating stalls, or Ayutthaya, the ancient capital city of Siam, filled with temples and palaces dating back to the 15th and 16th centuries. If you already have some ideas for your trip, let us help you to customize your tour to meet your needs and make sure that you have a fantastic time while in Thailand.
 </h1>
@@ -125,9 +125,29 @@ export default function About() {
 <h1 className='detailabout2'>
 Beyond the Trails is a tour operator registered in Thailand and run by experienced locals. Our goal is to connect our clients with the genuine local culture of Bangkok. We want you to witness both the highlights and hidden gems that make up our home city of Bangkok and its surrounding areas and also further afield in Thailand. Thailand has many beautiful and exotic spots for you to see and every tour is guaranteed private and organized exclusively for you. Alternatively, you can design your own trip and let us help you to bring it to life by organizing accommodation and transfers for you. On your tour, you will come across the ´must see´ highlights of Bangkok and also, the rarely visited, but equally fascinating places in Bangkok that only the locals know about. Join us and let us introduce you to the authentic Bangkok!
 
-</h1>
-</div>
+</h1></div>
+{/* </div> */}
 </Col>
+</Row>
+
+
+
+<Row className='detailabout3'>
+<h1 className='detailabout4'>
+                   Let Beyond the Trails be your first choice for unique private tours in and around Bangkok. For example, Thaka floating market, where you can immerse yourself in the local culture as the neighbourhood vendors ply their wares from their floating stalls, or Ayutthaya, the ancient capital city of Siam, filled with temples and palaces dating back to the 15th and 16th centuries. If you already have some ideas for your trip, let us help you to customize your tour to meet your needs and make sure that you have a fantastic time while in Thailand.
+</h1><br></br>
+         <div className='box5'>
+          <img
+        // style={{ width: "100%" , height: "650px" }}
+        className='about3'
+                src={about2}
+                alt="First slide"
+              /></div>
+               <br></br>
+<h1 className='detailabout3'>
+Beyond the Trails is a tour operator registered in Thailand and run by experienced locals. Our goal is to connect our clients with the genuine local culture of Bangkok. We want you to witness both the highlights and hidden gems that make up our home city of Bangkok and its surrounding areas and also further afield in Thailand. Thailand has many beautiful and exotic spots for you to see and every tour is guaranteed private and organized exclusively for you. Alternatively, you can design your own trip and let us help you to bring it to life by organizing accommodation and transfers for you. On your tour, you will come across the ´must see´ highlights of Bangkok and also, the rarely visited, but equally fascinating places in Bangkok that only the locals know about. Join us and let us introduce you to the authentic Bangkok!
+
+</h1>
 </Row>
 {/* </div> */}
 </div>
