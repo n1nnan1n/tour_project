51ef3a1699f11cefe40aa2bfd4e553872edbfe52
toon63 <kannatthanan@gmail.com>
2023-12-26 23:22:21 +0700

    first_time

--- src/AdHome.js
+++ src/AdHome.js
@@ -1,56 +1,202 @@
-import AnalyticEcommerce from './components/cards/statistics/AnalyticEcommerce';
-import React, { useState } from 'react';
+// import AnalyticEcommerce from './components/cards/statistics/AnalyticEcommerce';
+// import React, { useState } from 'react';
+// import { AppBar, Typography, Grid } from '@mui/material';
+// import MainCard from './components/MainCard';
+// import OrdersTable from './pages/dashboard/OrdersTable';
+// import Toolbar from '@mui/material/Toolbar';
+// const drawerWidth = 240;
+
+// function AdHome() {
+//   const [slot, setSlot] = useState('week');
+
+//   return (
+//     <>
+//       <AppBar position="fixed" sx={{ width: `calc(100% - ${drawerWidth}px)`, ml: `${drawerWidth}px` }} >
+//         <Toolbar>
+//           <Typography variant="h6" noWrap component="div">
+//             Home
+//           </Typography>
+//         </Toolbar>
+//       </AppBar>
+      
+
+//       <Grid container rowSpacing={3.5} columnSpacing={2.75} style={{marginLeft:'250px',maxWidth:'',width:"150vh"}}>
+      
+// {/*       
+//       <Grid item xs={12} sm={6} md={4} lg={3}>
+//         <AnalyticEcommerce title="Total Page Views" count="4,42,236" percentage={59.3} extra="35,000" />
+//       </Grid>
+//       <Grid item xs={12} sm={6} md={4} lg={3}>
+//         <AnalyticEcommerce title="Total Users" count="78,250" percentage={70.5} extra="8,900" />
+//       </Grid>
+//       <Grid item xs={12} sm={6} md={4} lg={3}>
+//         <AnalyticEcommerce title="Total Order" count="18,800" percentage={27.4} isLoss color="warning" extra="1,943" />
+//       </Grid>
+//       */}
+
+//       <Grid item md={8} sx={{ display: { sm: 'none', md: 'block', lg: 'none' } }} />
+
+//         {/* row 3 */}
+//         <Grid item xs={12} md={7} lg={8}>
+//           <Grid container alignItems="center" justifyContent="space-between">
+//             <Grid item>
+//               <Typography variant="h4">Recent Orders</Typography>
+//             </Grid>
+//             <Grid item />
+//           </Grid>
+//           <MainCard sx={{ mt: 2 }} content={false}>
+//             <OrdersTable />
+//           </MainCard>
+//         </Grid>
+//       </Grid>
+//     </>
+//   );
+// }
+
+// export default AdHome;
+
+  import * as React from 'react';
+import Paper from '@mui/material/Paper';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TablePagination from '@mui/material/TablePagination';
+import TableRow from '@mui/material/TableRow';
 import { AppBar, Typography, Grid } from '@mui/material';
 import MainCard from './components/MainCard';
 import OrdersTable from './pages/dashboard/OrdersTable';
 import Toolbar from '@mui/material/Toolbar';
 const drawerWidth = 240;
+const columns = [
+  { id: 'name', label: 'Name', minWidth: 170 },
+  { id: 'nametour', label: 'Nametour', minWidth: 170 },
+  { id: 'person', label: 'Person', minWidth: 170 },
+  { id: 'date', label: 'Date', minWidth: 170 },
+  { id: 'price', label: 'price', minWidth: 170 },
+  { id: 'status', label: 'status', minWidth: 170 },
+  // { id: 'code', label: 'ISO\u00a0Code', minWidth: 100 },
+  // {
+  //   id: 'population',
+  //   label: 'Population',
+  //   minWidth: 170,
+  //   align: 'right',
+  //   format: (value) => value.toLocaleString('en-US'),
+  // },
+  // {
+  //   id: 'size',
+  //   label: 'Size\u00a0(km\u00b2)',
+  //   minWidth: 170,
+  //   align: 'right',
+  //   format: (value) => value.toLocaleString('en-US'),
+  // },
+  // {
+  //   id: 'density',
+  //   label: 'Density',
+  //   minWidth: 170,
+  //   align: 'right',
+  //   format: (value) => value.toFixed(2),
+  // },
+];
 
-function AdHome() {
-  const [slot, setSlot] = useState('week');
+function createData(name, nametour,person,date,price,status) {
+  // const density = population / size;
+  return { name, nametour, person, date, price, status };
+}
+
+const rows = [
+  createData('India', 'IN', 1324171354, 3287263),
+  createData('China', 'CN', 1403500365, 9596961),
+  createData('Italy', 'IT', 60483973, 301340),
+  createData('United States', 'US', 327167434, 9833520),
+  createData('Canada', 'CA', 37602103, 9984670),
+  createData('Australia', 'AU', 25475400, 7692024),
+  createData('Germany', 'DE', 83019200, 357578),
+  createData('Ireland', 'IE', 4857000, 70273),
+  createData('Mexico', 'MX', 126577691, 1972550),
+  createData('Japan', 'JP', 126317000, 377973),
+  createData('France', 'FR', 67022000, 640679),
+  createData('United Kingdom', 'GB', 67545757, 242495),
+  createData('Russia', 'RU', 146793744, 17098246),
+  createData('Nigeria', 'NG', 200962417, 923768),
+  createData('Brazil', 'BR', 210147125, 8515767),
+];
+
+
+export default function AdHome() {
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
 
   return (
     <>
-      <AppBar position="fixed" sx={{ width: `calc(100% - ${drawerWidth}px)`, ml: `${drawerWidth}px` }} >
-        <Toolbar>
-          <Typography variant="h6" noWrap component="div">
-            Home
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      
-
-      <Grid container rowSpacing={3.5} columnSpacing={2.75} style={{marginLeft:'250px',maxWidth:'',width:"150vh"}}>
-      
-{/*       
-      <Grid item xs={12} sm={6} md={4} lg={3}>
-        <AnalyticEcommerce title="Total Page Views" count="4,42,236" percentage={59.3} extra="35,000" />
-      </Grid>
-      <Grid item xs={12} sm={6} md={4} lg={3}>
-        <AnalyticEcommerce title="Total Users" count="78,250" percentage={70.5} extra="8,900" />
-      </Grid>
-      <Grid item xs={12} sm={6} md={4} lg={3}>
-        <AnalyticEcommerce title="Total Order" count="18,800" percentage={27.4} isLoss color="warning" extra="1,943" />
-      </Grid>
-      */}
-
-      <Grid item md={8} sx={{ display: { sm: 'none', md: 'block', lg: 'none' } }} />
-
-        {/* row 3 */}
-        <Grid item xs={12} md={7} lg={8}>
-          <Grid container alignItems="center" justifyContent="space-between">
-            <Grid item>
-              <Typography variant="h4">Recent Orders</Typography>
-            </Grid>
-            <Grid item />
-          </Grid>
-          <MainCard sx={{ mt: 2 }} content={false}>
-            <OrdersTable />
-          </MainCard>
-        </Grid>
-      </Grid>
+    <AppBar position="fixed" sx={{ width: `calc(100% - ${drawerWidth}px)`, ml: `${drawerWidth}px` }} >
+       <Toolbar>
+       <Typography variant="h6" noWrap component="div">
+          Home
+               </Typography>
+             </Toolbar>
+          </AppBar>
+         
+    <Paper sx={{ width: '80%', overflow: 'hidden' ,marginLeft:'18%'}}> <h1>Order</h1>
+      <TableContainer sx={{ maxHeight: 440 }}>
+        <Table stickyHeader aria-label="sticky table">
+          <TableHead>
+            <TableRow>
+              {columns.map((column) => (
+                <TableCell
+                  key={column.id}
+                  align={column.align}
+                  style={{ minWidth: column.minWidth }}
+                >
+                  {column.label}
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((row) => {
+                return (
+                  <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+                    {columns.map((column) => {
+                      const value = row[column.id];
+                      return (
+                        <TableCell key={column.id} align={column.align}>
+                          {column.format && typeof value === 'number'
+                            ? column.format(value)
+                            : value}
+                        </TableCell>
+                      );
+                    })}
+                  </TableRow>
+                );
+              })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[10, 25, 100]}
+        component="div"
+        count={rows.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
+    </Paper>
     </>
   );
 }
 
-export default AdHome;
+
--- src/Review.js
+++ src/Review.js
@@ -1,11 +1,182 @@
 import React, { Component } from 'react'
+import Paper from '@mui/material/Paper';
+import Toolbar from '@mui/material/Toolbar';
+import {  Typography, Grid ,Box} from '@mui/material';
+import AppBar from '@mui/material/AppBar';
+import TextField from '@mui/material/TextField';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import { useEffect, useState } from "react";
 
-export class Review extends Component {
-  render() {
+import Stack from '@mui/material/Stack';
+import PropTypes from 'prop-types';
+import { styled } from '@mui/material/styles';
+import Rating from '@mui/material/Rating';
+import SentimentVeryDissatisfiedIcon from '@mui/icons-material/SentimentVeryDissatisfied';
+import SentimentDissatisfiedIcon from '@mui/icons-material/SentimentDissatisfied';
+import SentimentSatisfiedIcon from '@mui/icons-material/SentimentSatisfied';
+import SentimentSatisfiedAltIcon from '@mui/icons-material/SentimentSatisfiedAltOutlined';
+import SentimentVerySatisfiedIcon from '@mui/icons-material/SentimentVerySatisfied';
+
+import Button from '@mui/material/Button';
+import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+const VisuallyHiddenInput = styled('input')({
+    clip: 'rect(0 0 0 0)',
+    clipPath: 'inset(50%)',
+    height: 1,
+    overflow: 'hidden',
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    whiteSpace: 'nowrap',
+    width: 1,
+  });
+  
+export default function Review() {
+    
+  const [tour, settour] = useState('');  
+    const handleChange = (event) => {
+      settour(event.target.value);
+    };
+    const drawerWidth = 240;
+    const StyledRating = styled(Rating)(({ theme }) => ({
+        '& .MuiRating-iconEmpty .MuiSvgIcon-root': {
+          color: theme.palette.action.disabled,
+        },
+      }));
+      
+      const customIcons = {
+        1: {
+          icon: <SentimentVeryDissatisfiedIcon color="error" />,
+          label: 'Very Dissatisfied',
+        },
+        2: {
+          icon: <SentimentDissatisfiedIcon color="error" />,
+          label: 'Dissatisfied',
+        },
+        3: {
+          icon: <SentimentSatisfiedIcon color="warning" />,
+          label: 'Neutral',
+        },
+        4: {
+          icon: <SentimentSatisfiedAltIcon color="success" />,
+          label: 'Satisfied',
+        },
+        5: {
+          icon: <SentimentVerySatisfiedIcon color="success" />,
+          label: 'Very Satisfied',
+        },
+      };
+      
+      function IconContainer(props) {
+        const { value, ...other } = props;
+        return <span {...other}>{customIcons[value].icon}</span>;
+      }
+      
+      IconContainer.propTypes = {
+        value: PropTypes.number.isRequired,
+      };
+      
     return (
-      <div>Review</div>
+        <>
+        <AppBar position="fixed" sx={{ width: `calc(100% - ${drawerWidth}px)`, ml: `${drawerWidth}px` }} >
+           <Toolbar>
+           <Typography variant="h6" noWrap component="div">
+              Editor Review
+                   </Typography>
+                 </Toolbar>
+              </AppBar>
+              <Box
+      sx={{
+        display: 'flex',
+        flexWrap: 'wrap',
+        marginLeft:'20%',
+        '& > :not(style)': {
+          m: 1,
+          width: '90%',
+          height: 128,
+        },
+      }}
+    >
+    
+      <Paper elevation={3} style={{padding:'10px',height:'100%'}} >
+      <TextField
+          id="outlined-multiline-flexible"
+          label="Review title"
+          multiline
+          maxRows={4}
+          style={{float:'left',width:'49%',marginBottom:'10px'}}
+        />
+        <TextField
+          id="outlined-multiline-flexible"
+          label="User Name"
+          multiline
+          maxRows={4}
+          style={{float:'right',width:'49%',marginBottom:'10px'}}
+        />
+
+   
+      <FormControl fullWidth style={{width:'49%',float:'left'}}>
+        <InputLabel id="demo-simple-select-label">Tour</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={tour}
+          label="Tour"
+          onChange={handleChange}
+        //   style={{marginBottom:'10px'}}
+        >
+              <MenuItem value={'Bangkok grand tour'}>Bangkok grand tour</MenuItem>
+                <MenuItem value={'bangkok unseen tour'}>bangkok unseen tour</MenuItem>
+                <MenuItem value={'bkk Instargram/Tiktok'}>bkk Instargram/Tiktok</MenuItem>
+                <MenuItem value={'The ultimate of the floating market'}>The ultimate of the floating market</MenuItem>
+                <MenuItem value={'Half day floating market'}>Half day floating market</MenuItem>
+                <MenuItem value={'The sacred tattoo tour'}>The sacred tattoo tour</MenuItem>
+                <MenuItem value={'Ayutthaya highlight tour'}>Ayutthaya highlight tour</MenuItem>
+                <MenuItem value={'The scenic farm tour'}>The scenic farm tour</MenuItem>
+                <MenuItem value={'Cooking class'}>Cooking class</MenuItem>
+         
+        </Select>
+      </FormControl>
+      {/* <Stack spacing={1} style={{float:'right'}}>
+      <Rating name="half-rating" defaultValue={2.5} precision={0.5} />
+      
+    </Stack> */}
+    
+    <StyledRating
+      name="highlight-selected-only"
+    //   defaultValue={0}
+      IconContainerComponent={IconContainer}
+      getLabelText={(value) => customIcons[value].label}
+      highlightSelectedOnly
+      style={{marginTop:'15px',marginLeft:'20px',height:'42px'}}
+    />
+     <TextField
+          id="outlined-multiline-static"
+          label="Multiline"
+          multiline
+          rows={4}
+          defaultValue="Default Value"
+          style={{float:'left',marginTop:'10px',width:'100%'}}
+        />
+        {/* <Paper elevation={3} style={{width:'49%',float:'right',marginTop:"10px",height:'100%',padding:'4%'}}>
+        <Button component="label" variant="contained" startIcon={<CloudUploadIcon />}>
+      Upload file
+      <VisuallyHiddenInput type="file" />
+    </Button>
+    </Paper> */}
+    
+<Button variant="outlined" color="error" style={{marginTop:'10px',marginRight:"10px",float:'left'}}>
+ Cancle
+</Button>
+    <Button variant="contained" color="success" style={{marginTop:'10px',float:'right'}}>
+ Done
+</Button>
+        </Paper>
+    </Box>
+     
+      </>
     )
   }
-}
-
-export default Review
