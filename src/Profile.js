653c7923faab8f5d5be38d182a9d42d31d88449b
u o o ʇ <admin@192.168.1.40>
2024-01-04 12:15:38 +0700

    first_time

--- src/Profile.js
+++ src/Profile.js
@@ -5,7 +5,7 @@ import { faUser } from "@fortawesome/free-solid-svg-icons";
 import Editpro from './Editpro';
 import { Button } from 'react-bootstrap';
 import axios from 'axios';
-import { Col } from 'react-bootstrap';
+import Col from 'react-bootstrap/Col';
 import Row from "react-bootstrap/Row";
 
 import {
@@ -14,6 +14,7 @@ import {
   MDBContainer,
   MDBRow,
   MDBCard,
+  p,
   MDBCardText,
   MDBCardBody,
   MDBCardImage,
@@ -158,7 +159,7 @@ export default function Profile() {
                <div style={{ width: '100%',textAlign:'center',marginTop:'80px'}}>
              
                            <MDBTypography tag="h1">{profileDetail.p_title} {profileDetail.p_firstname} {profileDetail.p_lastname}</MDBTypography>
-                            <MDBCardText>{profileDetail.p_nation}</MDBCardText> 
+                           < MDBCardText>{profileDetail.p_nation}</ MDBCardText> 
                            <Button href='Editpro'>Edit profile</Button>
                             
                           </div>
@@ -174,73 +175,73 @@ export default function Profile() {
           <MDBCardBody>
             <MDBRow>
               <MDBCol sm="3">
-                <MDBCardText className='prodetail'>Email :</MDBCardText>
+               < MDBCardText className='prodetail'>Email :</ MDBCardText>
               </MDBCol>
               <MDBCol sm="9">
-                <MDBCardText className="text-muted">{profileDetail.p_email}</MDBCardText>
+               < MDBCardText className="text-muted">{profileDetail.p_email}</ MDBCardText>
               </MDBCol>
             </MDBRow>
             <hr />
             <MDBRow>
               <MDBCol sm="3">
-                <MDBCardText className='prodetail'>Phone :</MDBCardText>
+               < MDBCardText className='prodetail'>Phone :</ MDBCardText>
               </MDBCol>
               <MDBCol sm="9">
-                <MDBCardText className="text-muted"> {profileDetail.p_phone}</MDBCardText>
+               < MDBCardText className="text-muted"> {profileDetail.p_phone}</ MDBCardText>
               </MDBCol>
             </MDBRow>
             <hr />
             <MDBRow>
               <MDBCol sm="3">
-                <MDBCardText className='prodetail'>Birthdate :</MDBCardText>
+               < MDBCardText className='prodetail'>Birthdate :</ MDBCardText>
               </MDBCol>
               <MDBCol sm="9">
-                <MDBCardText className="text-muted">{profileDetail.p_birth}</MDBCardText>
+               < MDBCardText className="text-muted">{profileDetail.p_birth}</ MDBCardText>
               </MDBCol>
             </MDBRow>
             <hr />
             <MDBRow>
               <MDBCol sm="3">
-                <MDBCardText className='prodetail'>National : </MDBCardText>
+               < MDBCardText className='prodetail'>National : </ MDBCardText>
               </MDBCol>
               <MDBCol sm="9">
-                <MDBCardText className="text-muted">{profileDetail.p_nation}</MDBCardText>
+               < MDBCardText className="text-muted">{profileDetail.p_nation}</ MDBCardText>
               </MDBCol>
             </MDBRow>
             <hr />
             <MDBRow>
               <MDBCol sm="3">
-                <MDBCardText className='prodetail'>Passport Number :</MDBCardText>
+               < MDBCardText className='prodetail'>Passport Number :</ MDBCardText>
               </MDBCol>
               <MDBCol sm="9">
-                <MDBCardText className="text-muted">{profileDetail.p_passport}</MDBCardText>
+               < MDBCardText className="text-muted">{profileDetail.p_passport}</ MDBCardText>
               </MDBCol>
             </MDBRow>
             <hr />
             <MDBRow>
               <MDBCol sm="3">
-                <MDBCardText className='prodetail'>Passport Expire :</MDBCardText>
+               < MDBCardText className='prodetail'>Passport Expire :</ MDBCardText>
               </MDBCol>
               <MDBCol sm="9">
-                <MDBCardText className="text-muted">{profileDetail.p_passport_exp}</MDBCardText>
+               < MDBCardText className="text-muted">{profileDetail.p_passport_exp}</ MDBCardText>
               </MDBCol>
             </MDBRow>
             <hr />
             <MDBRow>
               <MDBCol sm="3">
-                <MDBCardText className='prodetail'>Food Allergy :</MDBCardText>
+               < MDBCardText className='prodetail'>Food Allergy :</ MDBCardText>
               </MDBCol>
               <MDBCol sm="9">
-                <MDBCardText className="text-muted">{profileDetail.p_food_allergy}</MDBCardText>
+               < MDBCardText className="text-muted">{profileDetail.p_food_allergy}</ MDBCardText>
               </MDBCol>
             </MDBRow>
             <hr />
             <MDBRow>
               <MDBCol sm="3">
-                <MDBCardText className='prodetail'>Requirement :</MDBCardText>
+               < MDBCardText className='prodetail'>Requirement :</ MDBCardText>
               </MDBCol>
               <MDBCol sm="9">
-                <MDBCardText className="text-muted">{profileDetail.p_special}</MDBCardText>
+               < MDBCardText className="text-muted">{profileDetail.p_special}</ MDBCardText>
               </MDBCol>
             </MDBRow>
             <hr />
@@ -251,24 +252,31 @@ export default function Profile() {
                        {Array.isArray(orders) && orders.length > 0 ? (
                          orders.map((order) => (
                         
-          <MDBCardBody>
-                      <div key={order._id} className="p-4" style={{ backgroundColor: 'lightgray',marginBottom:'20px'}}>
-                              <MDBCardText className="font-italic mb-1 prodetail1">Order id: </MDBCardText><MDBCardText className="font-italic mb-1 prodetail2">{order._id}</MDBCardText>
-                              <div style={{float:'none'}}>
-                                <img src={order.tour_image} alt="Tour" style={{ maxWidth: '25%',maxHeight: '30%',position:'absolute' , marginTop:'-52px'}} />
-                             </div>
-                              <MDBCardText className="font-italic mb-1 prodetail1">Order date: </MDBCardText> <MDBCardText className="font-italic mb-1 prodetail2">{order.formattedCreatedAt}</MDBCardText>
-                              <MDBCardText className="font-italic mb-1 prodetail1">Tour name: </MDBCardText> <MDBCardText className="font-italic mb-1 prodetail2">{order.tour_name}</MDBCardText>
-                             <MDBCardText className="font-italic mb-1 prodetail1">Tour date: </MDBCardText>  <MDBCardText className="font-italic mb-1 prodetail2"> {order.formattedTourDate}</MDBCardText>
-                              <MDBCardText className="font-italic mb-1 prodetail1">Tour price:</MDBCardText>  <MDBCardText className="font-italic mb-1 prodetail2">{order.tour_price}</MDBCardText>
-                              <MDBCardText className="font-italic mb-1 prodetail1">Person:</MDBCardText>  <MDBCardText className="font-italic mb-1 prodetail2"> {order.quantity}</MDBCardText>
-                             <MDBCardText className="font-italic mb-1 prodetail1">Total price: </MDBCardText><MDBCardText className="font-italic mb-1 prodetail2"> {order.total_price}</MDBCardText>
-                           </div>
+          // <MDBCardBody>
+                      <div key={order._id}  style={{ backgroundColor: 'lightgray',marginBottom:'20px'}}>
+                        <Row>
+                           <Col>
+                             <div className='boximgor'>
+                                <img src={order.tour_image} alt="Tour" className='imgorder' />
+                             </div>  
+                             </Col> <Col style={{clear:'both',marginTop:'10px'}}>
+                             < MDBCardText className="font-italic mb-1 prodetail1">Order id: </ MDBCardText>< MDBCardText className="font-italic mb-1 prodetail2">{order._id}</ MDBCardText>
+                             
+                             < MDBCardText className="font-italic mb-1 prodetail1">Order date: </ MDBCardText>< MDBCardText className="font-italic mb-1 prodetail2">{order.formattedCreatedAt}</ MDBCardText>
+                             < MDBCardText className="font-italic mb-1 prodetail1">Tour name: </ MDBCardText>< MDBCardText className="font-italic mb-1 prodetail2">{order.tour_name}</ MDBCardText>
+                            < MDBCardText className="font-italic mb-1 prodetail1">Tour date: </ MDBCardText> < MDBCardText className="font-italic mb-1 prodetail2"> {order.formattedTourDate}</ MDBCardText>
+                             < MDBCardText className="font-italic mb-1 prodetail1">Tour price:</ MDBCardText> < MDBCardText className="font-italic mb-1 prodetail2">{order.tour_price}</ MDBCardText>
+                             < MDBCardText className="font-italic mb-1 prodetail1">Person:</ MDBCardText> < MDBCardText className="font-italic mb-1 prodetail2"> {order.quantity}</ MDBCardText>
+                            < MDBCardText className="font-italic mb-1 prodetail1">Total price: </ MDBCardText>< MDBCardText className="font-italic mb-1 prodetail2"> {order.total_price}</ MDBCardText>
+                             </Col>
+                           
+                        </Row>
+                              </div>
 
-          </MDBCardBody>
+          // </MDBCardBody>
             ))
             ) : (
-              <MDBCardText className="font-italic mb-0">{orders.message}</MDBCardText>
+             < MDBCardText className="font-italic mb-0">{orders.message}</ MDBCardText>
             )}
             </>
             
